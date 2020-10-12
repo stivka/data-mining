601c19346b18ba1211d07883e0b29ca12db84c16
stivka@github.com <kaptenstiv@gmail.com>
2020-10-12 19:19:04 +0300

    Add part B non-binary table part

--- data preprocessing/binary_to_rows.py
+++ data preprocessing/binary_to_rows.py
@@ -1,4 +1,4 @@
-text_file = open("binary table.txt", "r")
+text_file = open("data preprocessing/binary table.txt", "r")
 raw_list = text_file.read().splitlines()
 print(str(raw_list))
 print(str(len(raw_list)) + ' rows of raw data' +'\n')
@@ -9,13 +9,21 @@ for line in raw_list:
 print(str(matrix))
 print('9 rows of addresses with 16 transaction columns' + '\n')
 
+output_file = open("D:\\Program Files\\Apriori\\input.txt", "w")
+
 transactions_for_address = []
 for address in range(9):
-    line = ('Address' + str(address + 1) + ': ')
+    line = ""
+    #line = ('Address' + str(address + 1) + ': ')
     for transaction in range(16):
         if (matrix[address][transaction] == '1'):
             transaction = 'Transaction' + str(transaction + 1) + ' '
             line += transaction
-    print(line)
+    output_file.write(line + "\n")
+    # print(line)
+output_file.close()
+
+output_file = open("D:\\Program Files\\Apriori\\input.txt", "r")
+print(output_file.read())
 
 # Also use numpy to transpose the data to have it as addresses for transactions
--- data preprocessing/non-binary_to_rows.py
+++ data preprocessing/non-binary_to_rows.py
@@ -1,10 +1,30 @@
-text_file = open("binary table.txt", "r")
+import codecs
+
+text_file = codecs.open("data preprocessing/non-binary table.txt", "r")
 raw_list = text_file.read().splitlines()
 print(str(raw_list))
-print(str(len(raw_list)) + ' rows of raw data' +'\n')
+#print(str(len(raw_list)) +  "rows of raw data" +"\n")
+
+# \xe2\x80\xa6 = HORIZONTAL ELLIPSIS
 
 matrix = []
 for line in raw_list:
-    matrix.append(line[9:].split(' '))
+    matrix.append(line.split(" "))
 print(str(matrix))
-print('9 rows of addresses with 16 transaction columns' + '\n')
+print(str(len(matrix)) + " rows of " + "6 columns.\n")
+
+# 8 columns for 14 transactions
+transaction = []
+dormant_less_than_30 = []
+dormant_between_30_360 = []
+dormant_more_than_360 = []
+value_10_plus = []
+
+value_less_than_1 = []
+More_than_50_transaction = []
+first_transaction_over_year_ago = []
+suspicious_Transaction = []
+
+#for row in matrix:
+#    for col in row:
+#        if  
